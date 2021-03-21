f495c9974d26c4679b1b2753eddf17de2f8a63d5
Simon-bit02 <braccio0203@gmail.com>
2021-03-21 19:23:13 +0100

    lampadina

--- src/App.tsx
+++ src/App.tsx
@@ -1,102 +1,77 @@
-import React, { useState, useEffect, useReducer } from 'react';
-import './App.css';
-import faker, { fake } from 'faker';
-import arrayMove from 'array-move';
+import React, {useState,useCallback,} from 'react';
+import _ from 'lodash/fp';
 import classNames from 'classnames';
-interface P { }
 
-interface Action {
-  type: 'add' | 'remove' | 'up' | 'down' | 'favorite';
-  payload?: {};
+interface Event {
+  status: 'LAMPADINA_SPENTA' | 'LAMPADINA_ACCESA';
+  timestamp: Date;
 }
 
+function App() {
+  const [events, setEvents] = useState<Event[]>([
+    { status: 'LAMPADINA_SPENTA', timestamp: new Date() },
+  ]);
 
-function reducer(index: number, people: string[], action: Action) {
-  switch (action) {
-    case 'remove':
-      const result = people.filter((person, jdx) => {
-        if (index === jdx) {
-          return false;
-        } else {
-          return true;
-        }
-      });
-      return result;
+  function BtnOn(p: Event[]) {
+    const style = classNames(
+      'px-4 py-3 mr-2 rounded disabled:opacity-50 disabled:cursor-not-allowed bg-green-500',
+    );
 
-    case 'add':
-      const newPeople = faker.name.findName();
-      ([...people, newPeople]);
-      return newPeople;
-    case 'up':
-      var i = index;
-      if (index != 0)
-        people = arrayMove(people, index, i - 1);
-      return people;
-    case 'down':
-      var i = index;
-      if (index != 0)
-        people = arrayMove(people, index, i + 1);
-      return people;
+    return (
+      <button
+        className={style}
+        onClick={() => LightOn(p)}
+      >
+        Acceso
+      </button>
+    );
   }
-}
 
+  function BtnOff(p: Event[]) {
+    const style = classNames(
+      'px-4 py-3 mr-2 rounded disabled:opacity-50 disabled:cursor-not-allowed bg-red-500',
+    );
 
+    return (
+      <button
+        className={style}
+        onClick={() => LightOff(p)}
+      >
+        Spento
+      </button>
+    );
+  }
 
-function App(props: P) {
-  const [people, dispatch] = useReducer(reducer, new Array(20).fill({}).map((d) => faker.name.findName())
+  const LightOn = useCallback(
+    (p: Event[]) => {
+      if (p.status === 'LAMPADINA_SPENTA') {
+        setEvents([
+          ...events,
+          { status: 'LAMPADINA_ACCESA', timestamp: new Date() },
+        ]);
+      }
+    },
+    [events],
   );
 
-  /*const addPeople = ()=> {
-    const newPeople = faker.name.findName();
-    setPeople([...people, newPeople]);
-  };
-
-  function removePeople (index: number) {
-    const result = people.filter((person, jdx) => {
-      if (index === jdx) {
-        return false;
-      } else {
-        return true;
+  const LightOff = useCallback(
+    (p: Event[]) => {
+      if (p.status === 'LAMPADINA_ACCESA') {
+        setEvents([
+          ...events,
+          { status: 'LAMPADINA_SPENTA', timestamp: new Date() },
+        ]);
       }
-    });
-    setPeople(result);
-  }
-
-  function moveUp(index: number){
-      var i = index;
-      if(index!=0)
-      setPeople(arrayMove(people, index, i-1 ));
-  }
-
-  function moveDown(index: number){
-      var i = index;
-      setPeople(arrayMove(people, index, i+1 ));
-  }
-
-  function favoritePeople (index: number){
-    const favorite = people.map((person, jdx))
-      <div
-        className={classNames('p-2 bg-blue-200 hover:bg-gray-200 cursor-pointer', {
-          'text-red-800 bg-gray-50': index==
-      })}/>
-    return(
-      
-    );
-  }*/
+    },
+    [events],
+  );
 
   return (
-    <div className="p-3">
-      {people.map((d, index) => (
-        <div className="flex py-1">
-          <div className="flex-grow">{d}</div>
-          <button className="bg-gray-400 ml-2" key={index} >Imposta come preferito</button>
-          <button className="bg-red-500 ml-2" key={index} onClick={() => dispatch('remove')} >Remove</button>
-          <button className="bg-gray-400 ml-2" key={index} onClick={() => dispatch('up')}>Up</button>
-          <button className="bg-gray-400 ml-2" key={index} onClick={() => dispatch('down')}>Down</button>
-        </div>
-      ))}
-      <button className="bg-blue-400" onClick={() => dispatch('add')}>Add person</button>
-    </div>
+    
+      <div className="flex-grow justify-center flex m-4">
+        <BtnOn {...events} />
+        <BtnOff {...events} />
+      </div>
   );
 }
 
